61652614433a97e30a544acd5673a770598c5b3d
Worachai Wuttiworachairung <worachai.wuttiworachairung@scbtechx.io>
2023-08-20 20:32:10 +0700

    Test github action

--- aws/main.tf
+++ aws/main.tf
@@ -91,8 +91,9 @@ resource "aws_instance" "http_server" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.http_server_sg.id]
 
-  for_each  = toset(data.aws_subnets.default_subnets.ids)
-  subnet_id = each.value
+  # for_each  = toset(data.aws_subnets.default_subnets.ids)
+  # subnet_id = each.value
+  subnet_id = data.aws_subnet_ids.default_subnet_ids.ids[0]
 
   connection {
     type        = "ssh"
