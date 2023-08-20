45cd033463a927b9387118645d28a032e006e30a
Worachai Wuttiworachairung <worachai.wuttiworachairung@scbtechx.io>
2023-08-20 23:12:17 +0700

    remove comment

--- aws/users/main.tf
+++ aws/users/main.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "s3" {
     bucket         = "dev-applications-backend-state-worachai"
-    # key            = "07-backend-state-users-dev"
     key            = "dev/07-backend-state/users/backend-state"
     region         = "us-east-1"
     dynamodb_table = "dev-applications-locks"
