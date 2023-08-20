4ea3750430336dc4955cf404852c1e58f30af557
Worachai Wuttiworachairung <worachai.wuttiworachairung@scbtechx.io>
2023-08-20 21:51:20 +0700

    use ssh key

--- aws/main.tf
+++ aws/main.tf
@@ -1,5 +1,3 @@
-
-
 provider "aws" {
   region = "us-east-1"
 }
@@ -8,6 +6,11 @@ resource "aws_default_vpc" "default" {
   tags = {
     Name = "Default VPC"
   }
+}
+
+resource "aws_key_pair" "default-ec2-public" {
+  key_name   = "default-ec2"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDLzRi9/hdhI2FGGiNAlFSc9z3Gchw2XiwXu1uyDGtp50iwWx4jLawU6vQv1z9sAjsYn88g7NTlsy5AgIqobEjxbQuFqWriTddIGblguDK7Zebkq3wr3niVPnqhhwuVrAmQ5NG5MUyOFnFf/SkOQ0/2M/OkMQiSK6DBdKt7+RPPjWE9xI2zSQQ1PEMPNr14Ptx+tGWCG9CuwqyGtkqUOlAELIroVe4CvqDelxQEDFQpS4J33v2G48ge+BOAoUYBxUCqxdTgj2Rc8t1hDqhmPl8Iz/jcBJoZzET5xM6S7HdozqSg/d2xewy+Rknml94qPh/D1U3w/iGNBeuiRCBAWg4tkRowVO2nRKIOTU4cMWMl8BY7Uk+c1cx1MPgL18YcrvCyFNOyWgY8bPFpHQ6uM6dfFGgtXDEyGLUXomknC29jnojdE081nTUndtDKUk3gZRb+FkTCAamI9Gx92CtSOTFZYzqGoANKQOzRCqp6o8NkeeuIUa5nGZFzm8gxb0DTQjc= worachai.wuttiworachairung@TXM092215A"
 }
 
 // HTTP Server -> SG
@@ -99,7 +102,7 @@ resource "aws_instance" "http_server" {
     type        = "ssh"
     host        = self.public_ip
     user        = "ec2-user"
-    private_key = var.TF_VAR_aws_key_pair
+    private_key = var.aws_key_pair
   }
 
   provisioner "remote-exec" {
