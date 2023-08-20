c0e36322ec97fe1d426f6a0d0078f9c41e600d57
Worachai Wuttiworachairung <worachai.wuttiworachairung@scbtechx.io>
2023-08-20 20:09:21 +0700

    Add aws

---
+++ aws/main.tf
@@ -0,0 +1,112 @@
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+
+// HTTP Server -> SG
+// SG -> -> 80 TCP, SSH -> 22 TCP, CIDR ["0.0.0.0/0"] means allow all
+
+resource "aws_security_group" "http_server_sg" {
+  name   = "http_server_sg"
+  vpc_id = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "http_server_sg"
+  }
+}
+
+
+resource "aws_security_group" "elb_sg" {
+  name   = "elb_sg"
+  vpc_id = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_elb" "elb" {
+  name            = "elb"
+  subnets         = data.aws_subnets.default_subnets.ids
+  security_groups = [aws_security_group.elb_sg.id]
+  instances       = values(aws_instance.http_server).*.id
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+}
+
+resource "aws_instance" "http_server" {
+  ami                    = data.aws_ami.aws-linux-2-latest.id
+  key_name               = "default-ec2"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.http_server_sg.id]
+
+  for_each  = toset(data.aws_subnets.default_subnets.ids)
+  subnet_id = each.value
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file(var.aws_key_pair)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install httpd -y",                                                                                //install httpd
+      "sudo service httpd start",                                                                                 //start httpd
+      "echo Welcome to in28minutes - Virtual Server is at ${self.public_dns} | sudo tee /var/www/html/index.html" //copy a file
+    ]
+  }
+}
+
--- azure/main.tf
+++ azure/main.tf
@@ -8,15 +8,15 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "e162899f-c381-4b35-b365-7bb62c579153"
-	tenant_id = "276c04a4-9964-4437-9690-331b175c05f0"
-	client_id = "0e38939d-d22e-4144-b325-0f4ee2664944"
-	client_secret = "hdo8Q~-mx9XugyTjVFnY1ih11bcyn0Ko5AyPtb9T"
+  # subscription_id = "e162899f-c381-4b35-b365-7bb62c579153"
+	# tenant_id = "276c04a4-9964-4437-9690-331b175c05f0"
+	# client_id = "0e38939d-d22e-4144-b325-0f4ee2664944"
+	# client_secret = "hdo8Q~-mx9XugyTjVFnY1ih11bcyn0Ko5AyPtb9T"
 
 	features {}
 }
 
 resource "azurerm_resource_group" "appgrp" {
 	name = "app-grp"
-	location = "Southeast Asia"
+	location = "West Europe"
 }
