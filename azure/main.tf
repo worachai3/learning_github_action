28b23649e3e2df65701d66cc5753aa532750ef72
benzWRC <worachai3@live.com>
2023-08-22 00:26:43 +0700

    Add some azure IaC

--- azure/main.tf
+++ azure/main.tf
@@ -13,6 +13,29 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "appgrp" {
-	name = "app-grp-2"
+	name = "app-grp"
 	location = "West Europe"
+}
+
+resource "azurerm_storage_account" "appstorewrc" {
+  name                     = "appstorewrc"
+  resource_group_name      = azurerm_resource_group.appgrp.name
+  location                 = azurerm_resource_group.appgrp.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind = "StorageV2"
+}
+
+resource "azurerm_storage_container" "data" {
+  name                  = "data"
+  storage_account_name  = azurerm_storage_account.appstorewrc.name
+  container_access_type = "blob"
+}
+
+resource "azurerm_storage_blob" "maintf" {
+  name                   = "maintf.tf"
+  storage_account_name   = azurerm_storage_account.appstorewrc.name
+  storage_container_name = azurerm_storage_container.data.name
+  type                   = "Block"
+  source                 = "main.tf"
 }
